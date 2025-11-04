568a98d711e2f0556d7dcc7c23e320c73abbe116
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-11-04 19:55:54 +0100

    parte superiore footer e parte inferiore, perchè divido il footer in due parti

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,16 @@
-function Header() {
+
+function Footer() {
     return (
         <footer>
+            <section className="footer-top">
 
+            </section>
+            <section className="footer-bottom">
+
+            </section>
         </footer>
     )
 }
 
-export default Header
+export default Footer
+
