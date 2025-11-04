f413cd6638e159cf1707e2cfd98f2fb05ff4f4c3
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-11-04 17:14:26 +0100

    aggiunto logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,26 @@
-
+import Logo from './LogoDC'
 
 function Header() {
     return (
         <header>
-            <ul className="nav justify-content-end">
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Active</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
+            <nav>
+                <img src={Logo} alt="" />
+                <ul className="nav justify-content-end">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Active</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </nav>
+
         </header>
     )
 }
---
+++ src/components/LogoDC.jsx
@@ -0,0 +1,11 @@
+
+import LogoDC from '../assets/img/dc-logo.png'
+
+export default function Logo() {
+
+    return (
+        <img className='logo' width="60" src={LogoDC} alt="" />
+    )
+
+
+}
