7683752d828ed6e1650d4ad96c619b37d1a4afdb
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-11-04 18:37:44 +0100

    fatto il main, aggiunto parte blu con loghi e scritte

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,53 @@
+import COMICS from '../assets/img/buy-comics-digital-comics.png';
+import MERCHANDISE from '../assets/img/buy-comics-merchandise.png';
+import SUBSCRIPTION from '../assets/img/buy-comics-subscriptions.png';
+import SHOP from '../assets/img/buy-comics-shop-locator.png';
+import POWER from '../assets/img/buy-dc-power-visa.svg';
 
 function Main() {
     return (
         <main>
             <section className="sfondo_main">
-                <div className="container pt-3 pb-3 link-light">
-                    <h1>
-                        Content goes here
-                    </h1>
+                <h1 className="container pt-3 pb-3 link-light">
+                    Content goes here
+                </h1>
+                <div className="blue-bar">
+                    <div className="container d-flex justify-content-between align-items-center py-4">
+                        <div className="d-flex align-items-center gap-2">
+                            <img src={COMICS} alt="digital comics" />
+                            <span>
+                                DIGITAL COMICS
+                            </span>
+                        </div>
+
+                        <div className="d-flex align-items-center gap-2">
+                            <img src={MERCHANDISE} />
+                            <span>
+                                DC MERCHANDISE
+                            </span>
+                        </div>
+
+                        <div className="d-flex align-items-center gap-2">
+                            <img src={SUBSCRIPTION} />
+                            <span>
+                                SUBSCRIPTION
+                            </span>
+                        </div>
+
+                        <div className="d-flex align-items-center gap-2">
+                            <img src={SHOP} />
+                            <span>
+                                COMIC SHOP LOCATOR
+                            </span>
+                        </div>
+
+                        <div className="d-flex align-items-center gap-2">
+                            <img src={POWER} />
+                            <span>
+                                DC POWER VISA
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </section>
 
